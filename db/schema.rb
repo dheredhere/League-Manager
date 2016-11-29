e924d602b9cf061bf7efb2d55fc87d06de8de5e7
dheredhere <dheredhere@gmail.com>
2016-11-28 19:01:17 -0800

    added user and league

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129024338) do
+ActiveRecord::Schema.define(version: 20161129025400) do
+
+  create_table "leagues", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
