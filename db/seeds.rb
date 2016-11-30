8728a687d8ee074cc9231ccc1516ded518630f70
dheredhere <dheredhere@gmail.com>
2016-11-29 21:46:22 -0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+#make leagues
+%w(Intramural Intermediate Competitive).each do |name|
+  League.create name: name, creator_id: 1 
+end
+
+#make players
+%w(Federer Nadal Murray Henman Djokovic).each do |name|
+  User.create name: name, email: name+"@league.com", password: 'password', password_confirmation: 'password', league_id: 2
+end
+
+
